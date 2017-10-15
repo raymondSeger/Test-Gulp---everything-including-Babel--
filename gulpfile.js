a579a348fccca2a96a2a2ba4ba1cd16b59ddcfff
Raymond Seger <raymondseger@live.com>
2017-10-15 15:56:01 +0700

    SASS htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
 
 // run gulp sass
 gulp.task('sass', function(){
     return gulp.src('dev/sass/*.scss')
         .pipe(sass()) // Using gulp-sass
         .pipe(gulp.dest('public/css'))
+});
+
+// run gulp htmlmin
+gulp.task('htmlmin', function() {
+    return gulp.src('dev/templates/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('public/templates'));
 });
